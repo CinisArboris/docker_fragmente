7767fe00e6e4c91e794fa239ebc6b3a876a6d0eb
Eyver Emilio Vargas Medina <eyver.evm@gmail.com>
2023-08-21 23:39:43 -0400

    update : lab config

---
+++ DDE-V1E1-004/semana_3.laboratorio_3/script.mariadb.bat
@@ -0,0 +1,28 @@
+:: Detener y eliminar contenedores basados en la imagen de mariadb
+FOR /F "tokens=*" %%i IN ('docker ps -a -q --filter "ancestor=mariadb"') DO (
+    docker stop %%i && docker rm %%i
+)
+
+:: Detener y eliminar contenedores con el nombre de mariadb
+FOR /F "tokens=*" %%i IN ('docker ps -a -q --filter "name=mariadb"') DO (
+    docker stop %%i && docker rm %%i
+)
+
+:: Eliminar imágenes de mariadb
+FOR /F "tokens=*" %%i IN ('docker images -q mariadb') DO (
+    docker rmi %%i
+)
+
+:: Eliminar carpeta compartida
+rd /s /q "%APPDATA%\share\*"
+
+:: Correr un nuevo contenedor de mariadb
+docker run -d ^
+--name mariadb ^
+--network redwp ^
+-v "%APPDATA%\share\:/var/lib/mysql" ^
+-e MARIADB_DATABASE=wpbase ^
+-e MARIADB_USER=wpuser ^
+-e MARIADB_PASSWORD=123456 ^
+-e MARIADB_ROOT_PASSWORD=123456 ^
+mariadb
