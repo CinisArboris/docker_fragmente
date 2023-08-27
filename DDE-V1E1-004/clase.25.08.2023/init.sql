be8853ce3dd4a7b3ac423b3623ec220fb4007cd7
Eyver Emilio Vargas Medina <eyver.evm@gmail.com>
2023-08-27 19:10:28 -0400

    update : fix code

--- DDE-V1E1-004/clase.25.08.2023/init.sql
+++
@@ -1,8 +0,0 @@
--- Crea el usuario
-CREATE USER 'mariauser'@'%' IDENTIFIED BY '123456';
-
--- Otorga todos los privilegios al usuario
-GRANT ALL PRIVILEGES ON *.* TO 'mariauser'@'%';
-
--- Refresca los privilegios
-FLUSH PRIVILEGES;
--- DDE-V1E1-004/clase.25.08.2023/script.a.bat
+++ DDE-V1E1-004/clase.25.08.2023/script.a.bat
@@ -2,6 +2,6 @@
 set COMPOSE_FILE=C:\Users\eyver\Documents\git\docker_fragmente\DDE-V1E1-004\clase.25.08.2023\compose.mariadb.yml
 
 docker pull mariadb:latest
-docker tag mariadb:latest mysql-custom
+docker tag mariadb:latest mariadb-custom
 
 docker-compose -f "%COMPOSE_FILE%" up -d
