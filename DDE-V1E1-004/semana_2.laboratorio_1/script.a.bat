0579d58830f3292a60e5da030a4a18eab96f819c
Eyver Emilio Vargas Medina <eyver.evm@gmail.com>
2023-08-27 08:16:39 -0400

    update : fix

--- DDE-V1E1-004/semana_2.laboratorio_1/script.a.bat
+++ DDE-V1E1-004/semana_2.laboratorio_1/script.a.bat
@@ -1,28 +1,28 @@
-# Obtener la imagen base (debian)
+:: Obtener la imagen base (debian)
 docker pull debian:bullseye
 
-# Asignar un tag a la imagen
+:: Asignar un tag a la imagen
 docker tag debian:bullseye i.lab1
 
-# Crear el contenedor
+:: Crear el contenedor
 docker run -d --name c.lab1 -p 100:80 i.lab1 tail -f /dev/null
 
-# Ingresar al contenedor
+:: Ingresar al contenedor
 docker exec -it c.lab1 bash
 
-# Actualizar la biblioteca de librerias
+:: Actualizar la biblioteca de librerias
 apt-get update
 
-# Instalar el servidor
+:: Instalar el servidor
 apt -y install apache2
 
-# Salir del contenedor
+:: Salir del contenedor
 exit
 
-# Iniciar los servicios
+:: Iniciar los servicios
 docker exec c.lab1 service apache2 start
 
-# Modificando el index.html
+:: Modificando el index.html
 echo ^<h1 style="text-align:center;"^>Nombre: Eyver Emilio Vargas Medina^</h1^> > index.html
 docker cp index.html c.lab1:/var/www/html/index.html
 del index.html
