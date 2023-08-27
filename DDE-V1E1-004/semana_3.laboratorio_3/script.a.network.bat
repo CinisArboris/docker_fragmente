12c80a5bc315842fe0508102a5a8e0defc54e407
Eyver Emilio Vargas Medina <eyver.evm@gmail.com>
2023-08-27 08:16:55 -0400

    update : step

--- DDE-V1E1-004/semana_3.laboratorio_3/script.a.network.bat
+++ DDE-V1E1-004/semana_3.laboratorio_3/script.a.network.bat
@@ -1,7 +1,27 @@
-docker network ls | findstr /I "r.lab3"
+@echo off
 
+:: Comprobar si la red r.lab3 existe
+docker network ls | findstr /I "r.lab3" >nul
 IF %ERRORLEVEL% EQU 0 (
+    echo La red "r.lab3" ya existe.
+
+    :: Obtener contenedores conectados a la red y desconectarlos
+    FOR /F "tokens=*" %%i IN ('docker network inspect r.lab3 --format "{{range .Containers}}{{.Name}} {{end}}"') DO (
+        FOR %%j IN (%%i) DO (
+            echo Desconectando el contenedor %%j de la red "r.lab3"...
+            docker network disconnect r.lab3 %%j
+        )
+    )
+
+    echo Eliminando la red "r.lab3"...
+    :: Eliminar la red
     docker network rm r.lab3
+) ELSE (
+    echo La red "r.lab3" no existe.
 )
 
+:: Crear la red r.lab3
+echo Creando la red "r.lab3"...
 docker network create r.lab3
+
+echo Listo.
