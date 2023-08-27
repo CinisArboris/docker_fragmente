7acfa920b1be8ecafb336114db50598bcaefa0f2
Eyver Emilio Vargas Medina <eyver.evm@gmail.com>
2023-08-27 19:09:57 -0400

    Update script.a.bat

--- DDE-V1E1-004/clase.18.08.2023/script.a.bat
+++ DDE-V1E1-004/clase.18.08.2023/script.a.bat
@@ -3,20 +3,20 @@
 :: Descargar la imagen base
 docker pull debian:bullseye
 
-:: Nueva imagen con el tag: default.imagen.debian.bullseye
-docker tag debian:bullseye default.imagen.debian.bullseye
+:: Nueva imagen con el tag: i.debian.18
+docker tag debian:bullseye i.debian.18
 
 :: Detener el contenedor si existe
-docker stop contenedor.bullseye 2>nul
+docker stop c.debian.18 2>nul
 
 :: Eliminar el contenedor si existe
-docker rm contenedor.bullseye 2>nul
+docker rm c.debian.18 2>nul
 
 :: Crear el contenedor
-docker run -d --name contenedor.bullseye default.imagen.debian.bullseye tail -f /dev/null
+docker run -d --name c.debian.18 i.debian.18 tail -f /dev/null
 
 :: Actualizar debian
-docker exec contenedor.bullseye apt-get update
+docker exec c.debian.18 apt-get update
 
 :: Instalar nmap
-docker exec contenedor.bullseye apt-get install -y nmap
+docker exec c.debian.18 apt-get install -y nmap
