1353c96fcc1992f1e0b38cd4f5191803d0071c88
Eyver Emilio Vargas Medina <eyver.evm@gmail.com>
2023-09-03 10:45:12 -0400

    update : fix lab

--- DDE-V1E1-004/clase.18.08.2023/script.a.bat
+++ DDE-V1E1-004/clase.18.08.2023/script.a.bat
@@ -1,22 +1,34 @@
-:: Personalizar una imagen docker base, para luego subir a dockerhub.
+:: Definir variables
+SET IMAGEN_BASE=debian:bullseye
+SET NOMBRE_IMAGEN_PERSONALIZADA=debian-custom
+SET VERSION_IMAGEN=v1.0.9
+SET NOMBRE_CONTENEDOR=c.debian.18
+SET NOMBRE_USUARIO_DOCKERHUB=cinisarboris
+SET REPOSITORIO_DOCKERHUB=dde-v1e1-004
 
 :: Descargar la imagen base
-docker pull debian:bullseye
+docker pull %IMAGEN_BASE%
 
-:: Nueva imagen con el tag: i.debian.18
-docker tag debian:bullseye i.debian.18
+:: Nueva imagen con el tag
+docker tag %IMAGEN_BASE% %NOMBRE_USUARIO_DOCKERHUB%/%NOMBRE_IMAGEN_PERSONALIZADA%:%VERSION_IMAGEN%
 
 :: Detener el contenedor si existe
-docker stop c.debian.18 2>nul
+docker stop %NOMBRE_CONTENEDOR% 2>nul
 
 :: Eliminar el contenedor si existe
-docker rm c.debian.18 2>nul
+docker rm %NOMBRE_CONTENEDOR% 2>nul
 
 :: Crear el contenedor
-docker run -d --name c.debian.18 i.debian.18 tail -f /dev/null
+docker run -d --name %NOMBRE_CONTENEDOR% %NOMBRE_USUARIO_DOCKERHUB%/%NOMBRE_IMAGEN_PERSONALIZADA%:%VERSION_IMAGEN% tail -f /dev/null
 
 :: Actualizar debian
-docker exec c.debian.18 apt-get update
+docker exec %NOMBRE_CONTENEDOR% apt-get update
 
 :: Instalar nmap
-docker exec c.debian.18 apt-get install -y nmap
+docker exec %NOMBRE_CONTENEDOR% apt-get install -y nmap
+
+:: Hacer commit del contenedor a una nueva imagen
+docker commit %NOMBRE_CONTENEDOR% %NOMBRE_USUARIO_DOCKERHUB%/%REPOSITORIO_DOCKERHUB%:%VERSION_IMAGEN%
+
+:: Subir a Docker Hub
+docker push %NOMBRE_USUARIO_DOCKERHUB%/%REPOSITORIO_DOCKERHUB%:%VERSION_IMAGEN%
